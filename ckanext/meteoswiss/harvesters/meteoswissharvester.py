09d1870ede9dbe71cdf781788d11158f69f61d84
Stefan Reinhard <sr@feinheit.ch>
2013-08-12 15:15:12 +0200

    remove unused code

--- ckanext/meteoswiss/harvesters/meteoswissharvester.py
+++ ckanext/meteoswiss/harvesters/meteoswissharvester.py
@@ -1,37 +1,26 @@
 #n -*- coding: utf-8 -*-
 
-import random
-import os
-import shutil
-import tempfile
-import zipfile
-from pprint import pprint
-from collections import defaultdict
 
-from ckan.lib.base import c
-from ckan import model
-from ckan.model import Session, Package
-from ckan.logic import ValidationError, NotFound, get_action, action
-from ckan.lib.helpers import json
 
-from ckanext.harvest.model import HarvestJob, HarvestObject, HarvestGatherError, HarvestObjectError
-from base import OGDCHHarvesterBase
+from ckanext.harvest.model import HarvestJob
+from ckanext.harvest.harvesters import HarvesterBase
 
-from ckanext.swisstopo.helpers import ckan_csw
 from ckanext.swisstopo.helpers import s3
 
 import logging
 log = logging.getLogger(__name__)
 
-class MeteoswissHarvester(OGDCHHarvesterBase):
+class MeteoswissHarvester(HarvesterBase):
     '''
     The harvester for meteoswiss
     '''
 
+
+
     def info(self):
         return {
             'name': 'meteoswiss',
-            'title': 'meteoswiss',
+            'title': 'METEOSWISS',
             'description': 'Harvests the meteoswiss data',
             'form_config_interface': 'Text'
         }
